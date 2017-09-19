672283fd2238f6375cd70a30aa94059e3918bd71
Doug Harper <doug@zewkey.com>
2017-09-19 11:59:25 -0500

    Basic Extensions - Color Struct and UIView Anchors Date Extension - formating dates

---
+++ Eratosthenes/CommonExtensions/BasicProject+Extensions.swift
@@ -0,0 +1,48 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by doug harper on 6/17/17.
+//  Copyright © 2017 Doug Harper. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  struct MyColor {
+    static let textField = UIColor(white: 0.0, alpha: 0.03)
+  }
+  
+  static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+    return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+  }
+}
+
+extension UIView {
+  func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+    
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    if let top = top {
+      topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+    }
+    if let left = left {
+      leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+    }
+    if let bottom = bottom {
+      bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+    }
+    if let right = right {
+      rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+    }
+    if width != 0 {
+      widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    if height != 0 {
+      heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+  }
+}
+
+
+
