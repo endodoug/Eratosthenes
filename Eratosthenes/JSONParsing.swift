179362cf75bf07f1bd2e5b82c37afa13ddab7d4e
Doug Harper <doug@zewkey.com>
2017-09-25 12:38:43 -0500

    Playing with JSON Parsing in Swift 4

--- Eratosthenes/JSONParsing.swift
+++ Eratosthenes/JSONParsing.swift
@@ -1,15 +1,14 @@
 //
-//  ViewController.swift
+//  File.swift
 //  Eratosthenes
 //
-//  Created by doug harper on 9/18/17.
+//  Created by doug harper on 9/25/17.
 //  Copyright © 2017 Doug Harper. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-  
+class JSONParsing: UIViewController {
   // http://api.letsbuildthatapp.com/jsondecodable/course
   // This guy ⬇︎ returns an array of courses
   // https://api.letsbuildthatapp.com/jsondecodable/course
@@ -17,7 +16,6 @@ class ViewController: UIViewController {
   // https://api.letsbuildthatapp.com/jsondecodable/website_description
   // Missing Data in JSON
   // https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields
-  
   
   // Create a model object that reflects what our JSON is
   struct Course: Decodable {
@@ -32,45 +30,41 @@ class ViewController: UIViewController {
     let description: String
     let courses: [Course]
   }
-
+  
+  @IBOutlet weak var label: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     let jsonURLString: String = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
     guard let url = URL(string: jsonURLString) else { return }
-
+    
     URLSession.shared.dataTask(with: url) { (data, response, err) in
       // check for err
       // could check for response status 200
       
       guard let data = data else { return }
-    
-//      let dataAsString = String(data: data, encoding: .utf8)
-//      print(dataAsString)
+      
+      //      let dataAsString = String(data: data, encoding: .utf8)
+      //      print(dataAsString)
       do {
         let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
         print("\(websiteDescription.name): \(websiteDescription.description) \n Course: \(websiteDescription.courses[0].name ?? "")")
-//        let courses = try
-//          JSONDecoder().decode([Course].self, from: data)
-//        print(courses)
-
+        //        let courses = try
+        //          JSONDecoder().decode([Course].self, from: data)
+        //        print(courses)
+        
       } catch let jsonErr {
         print("error deserializing json: ", jsonErr)
       }
-
-    }.resume()
-    
-//    let myCourse = Course(id: 1, name: "my course", link: "some link", imageUrl: "some image URL")
-//    print(myCourse)
+      
+      }.resume()
     
     
+    //    let myCourse = Course(id: 1, name: "my course", link: "some link", imageUrl: "some image URL")
+    //    print(myCourse)
+  
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+  
 }
 
