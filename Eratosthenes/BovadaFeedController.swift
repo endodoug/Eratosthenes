f53c0527fb8383191551c775eea49fe30458e149
Doug Harper <doug@zewkey.com>
2017-09-26 23:31:40 -0500

    Adjusted App Transport Security JSON as a String Dumped into the console from my URL.

---
+++ Eratosthenes/BovadaFeedController.swift
@@ -0,0 +1,42 @@
+//
+//  BovadaFeedController.swift
+//  Eratosthenes
+//
+//  Created by doug harper on 9/26/17.
+//  Copyright © 2017 Doug Harper. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/*: ## JSON Feeds Bovada
+ MMA -> http://sportsfeeds.bovada.lv/v1/feed?clientId=1556643&categoryCodes=1201&language=en
+ Politics -> http://sportsfeeds.bovada.lv/v1/feed?clientId=1556643&categoryCodes=22888&language=en
+ */
+
+struct Politics: Decodable {
+  let events: String?
+  let id: Int?
+}
+
+class BovadaFeedController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let jsonString: String = "http://sportsfeeds.bovada.lv/v1/feed?clientId=1556643&categoryCodes=22888&language=en"
+    guard let url = URL(string: jsonString) else { return }
+    
+    URLSession.shared.dataTask(with: url) { (data, response, err) in
+      // check for err
+      // could check for response status 200
+          guard let data = data else { return }
+      
+          let dataAsString = String(data: data, encoding: .utf8)
+          print(dataAsString)
+      
+      }.resume()
+    
+  }
+  
+}
